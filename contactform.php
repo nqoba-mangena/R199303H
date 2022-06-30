73339444677333a5db9201f4ac5a75d85a496968
nqoba-mangena <108475330+nqoba-mangena@users.noreply.github.com>
2022-06-30 16:15:20 +0200

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,39 @@
+<?php
+
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+
+
+$name = $_POST['userName'];
+$visitor_email = $_POST['userEmail'];
+$message = $_POST['content'];
+
+							//Validate first
+
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+$email_from = 'r199303h@msu.students.ac.zw'; 
+$email_subject = "New Form submission";
+$email_body = "You have received a new message from the user $name.\n".
+    "email address: $visitor_email\n".
+    "Here is the message:\n $message".
+    
+$to = "r199303h@msu.students.ac.zw"; 
+$headers = "From: $email_from \r\n";
+
+					//Send the email!
+
+mail($to,$email_subject,$email_body,$headers);
+
+				//done. redirect to thank-you page.
+
+header('Location: contact.html');
+  
+?> 
